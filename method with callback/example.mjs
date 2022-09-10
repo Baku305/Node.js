2581cd1a790e2de92a56d3e5881bff6a0d6ec008
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-09-10 14:49:29 +0200

    fix method with callback

--- method with callback/example.mjs
+++ method with callback/example.mjs
@@ -1,5 +1,18 @@
 import * as fs from "node:fs";
 
-fs.readFile("file-0.txt", { encoding: "utf-8" }, (error, data) =>
-  error ? console.error(error) : console.log(data)
-);
+// fs.readFile("file-0.txt", { encoding: "utf-8" }, (error, data) =>
+//   error ? console.error(error) : console.log(data)
+// );
+
+const data = "In this file was written somethings"
+
+fs.writeFile("someText.txt", data, (err) => {
+
+ if (err) {
+  console.log(err);
+ } else {
+  console.log("file written successfully");
+  console.log(fs.readFileSync("someText.txt", "utf8"));
+ }
+
+})
